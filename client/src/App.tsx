f6835d540ddb60e1fa131e91f2836e954740b636
boyka <abdelrahmanzaki747@gmail.com>
2023-07-18 01:42:34 +0300

    create navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
+import Navbar from "./components/navbar/Navbar"
+
 function App() {
+  const [selectedPage, setSelectedPage]= useState<string>("home")
   return (
-    <div className="app">app
+    <div className="app bg-gray-20">
+      <Navbar 
+        selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   )
 }
